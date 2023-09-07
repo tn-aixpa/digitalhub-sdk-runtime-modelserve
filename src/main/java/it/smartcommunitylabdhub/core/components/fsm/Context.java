d4f1bed11b0b03faaccedb1e079c505d72b29296
Luca Trubbiani <luca.trubbiani@gmail.com>
2023-09-07 13:13:59 +0200

    Delete deepCopy method in StateMachine Context class

--- src/main/java/it/smartcommunitylabdhub/core/components/fsm/Context.java
+++ src/main/java/it/smartcommunitylabdhub/core/components/fsm/Context.java
@@ -1,9 +1,9 @@
 package it.smartcommunitylabdhub.core.components.fsm;
 
+import java.io.Serializable;
 import java.util.*;
-import java.util.function.Supplier;
 
-class Context<C> {
+class Context<C> implements Serializable {
 	private Optional<C> value;
 
 	public Context(Optional<C> data) {
@@ -16,69 +16,5 @@ class Context<C> {
 
 	public void setValue(Optional<C> data) {
 		this.value = data;
-	}
-
-	public Context<C> deepCopy() {
-		if (value.isEmpty()) {
-			return new Context<>(Optional.empty()); // Handle empty Optional
-		} else if (value.get() instanceof Integer || value.get() instanceof Double
-				|| value.get() instanceof String || value.get() instanceof Boolean) {
-			// Handle primitive types and common immutable types
-			return new Context<>(value);
-		} else if (value.get() instanceof List<?>) {
-			// Handle deep copy for List
-			List<?> originalList = (List<?>) value.get();
-			List<Object> copiedList = new ArrayList<>();
-
-			for (Object element : originalList) {
-				copiedList.add(deepCopyObject(element));
-			}
-
-			return new Context<>(Optional.of((C) copiedList));
-		} else if (value.get() instanceof Map<?, ?>) {
-			// Handle deep copy for Map
-			Map<?, ?> originalMap = (Map<?, ?>) value.get();
-			Map<Object, Object> copiedMap = new HashMap<>();
-
-			for (Map.Entry<?, ?> entry : originalMap.entrySet()) {
-				Object keyCopy = deepCopyObject(entry.getKey());
-				Object valueCopy = deepCopyObject(entry.getValue());
-				copiedMap.put(keyCopy, valueCopy);
-			}
-
-			return new Context<>(Optional.of((C) copiedMap));
-		} else {
-			// Handle other custom object types
-			return new Context<>(Optional.of((C) deepCopyObject(value.get())));
-		}
-	}
-
-	private Object deepCopyObject(Object obj) {
-		if (obj == null) {
-			return null;
-		} else if (obj instanceof Context<?>) {
-			@SuppressWarnings("unchecked")
-			Context<C> contextObj = (Context<C>) obj;
-			return contextObj.deepCopy();
-		} else {
-			// Handle other objects and custom deep copy logic here
-			// For simplicity, assume they have a copy constructor
-			try {
-				Supplier<Object> supplier = () -> {
-					try {
-						// Assuming the object has a copy constructor (you may need to modify
-						// this)
-						return obj.getClass().getConstructor(obj.getClass()).newInstance(obj);
-					} catch (Exception e) {
-						e.printStackTrace();
-						return null;
-					}
-				};
-				return supplier.get();
-			} catch (Exception e) {
-				e.printStackTrace();
-				return null;
-			}
-		}
 	}
 }
