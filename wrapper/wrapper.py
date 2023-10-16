f5e7a921048dd6e1781814cdc8615cdebbe807ba
EvangMM <matteomartini92@yahoo.it>
2023-10-16 10:56:05 +0200

    fix on wrapper

--- wrapper/wrapper.py
+++ wrapper/wrapper.py
@@ -11,11 +11,20 @@ def main():
     Main function. Get run from backend and execute function.
     """
 
+    # Get run from backend.
     project = sdk.get_project(os.getenv("PROJECT_NAME"))
     run = sdk.get_run(project.metadata.name, os.getenv("RUN_ID"))
+
+    # Execute function in local mode.
     run.spec.local_execution = True
+
+    # Build and run.
     run.build()
     run.run()
+
+    # Reverse flag to remote mode and save.
+    run.spec.local_execution = False
+    run.save(run.id)
 
 if __name__ == "__main__":
     main()
