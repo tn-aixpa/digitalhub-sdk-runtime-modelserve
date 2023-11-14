9a5d4e1e07ed61dc3657a3d0443106cbdab2dec9
Luca Trubbiani <luca.trubbiani@gmail.com>
2023-11-14 14:03:41 +0100

    Convert DbtDataitem..into DataItemDataItemSpec that is now generic

--- src/main/java/it/smartcommunitylabdhub/core/models/entities/dataitem/specs/DataItemDataItemSpec.java
+++ src/main/java/it/smartcommunitylabdhub/core/models/entities/dataitem/specs/DataItemDataItemSpec.java
@@ -1,24 +1,23 @@
-package it.smartcommunitylabdhub.modules.dbt.models.specs;
+package it.smartcommunitylabdhub.core.models.entities.dataitem.specs;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import it.smartcommunitylabdhub.core.annotations.common.SpecType;
 import it.smartcommunitylabdhub.core.components.infrastructure.factories.specs.SpecEntity;
-import it.smartcommunitylabdhub.core.models.entities.dataitem.specs.DataItemBaseSpec;
 import lombok.Getter;
 import lombok.Setter;
 
 
 @Getter
 @Setter
-@SpecType(kind = "dbt", entity = SpecEntity.DATAITEM)
-public class DataItemDbtSpec extends DataItemBaseSpec<DataItemDbtSpec> {
+@SpecType(kind = "dataitem", entity = SpecEntity.DATAITEM)
+public class DataItemDataItemSpec extends DataItemBaseSpec<DataItemDataItemSpec> {
     @JsonProperty("raw_code")
     private String rawCode;
     @JsonProperty("compiled_code")
     private String compiledCode;
 
     @Override
-    protected void configureSpec(DataItemDbtSpec dataItemDbtSpec) {
+    protected void configureSpec(DataItemDataItemSpec dataItemDbtSpec) {
         super.configureSpec(dataItemDbtSpec);
 
         this.setRawCode(dataItemDbtSpec.getRawCode());
