49d6b741994a02930c5848aba843f0cbd06292ab
kazhamiakin <kazhamiakin@gmail.com>
2024-04-12 16:06:08 +0200

    Status class

--- core/modules/kfp/digitalhub_core_kfp/entities/workflows/status.py
+++ core/modules/kfp/digitalhub_core_kfp/entities/workflows/status.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from digitalhub_core_kfp.entities.functions.status import FunctionStatusKFP
 
 
-class WorkflowtatusKFP(FunctionStatusKFP):
+class WorkflowStatusKFP(FunctionStatusKFP):
     """
     Function KFP status.
     """
