c291a1d04d5bbcb7092449e7601f51d4b47e6e4e
EvangMM <matteomartini92@yahoo.it>
2024-10-23 13:44:29 +0200

    fix: removed implicit resolver timestamp on yaml import

--- digitalhub/utils/io_utils.py
+++ digitalhub/utils/io_utils.py
@@ -37,6 +37,42 @@ def write_yaml(filepath: str | Path, obj: dict | list[dict]) -> None:
 ##############################
 
 
+
+class NoDatesSafeLoader(yaml.SafeLoader):
+    """
+    Loader implementation to exclude implicit resolvers.
+
+    Taken from https://stackoverflow.com/a/37958106
+    """
+
+    @classmethod
+    def remove_implicit_resolver(cls, tag_to_remove: str) -> None:
+        """
+        Remove implicit resolvers for a particular tag
+        Takes care not to modify resolvers in super classes.
+        We want to load datetimes as strings, not dates, because we
+        go on to serialise as json which doesn't have the advanced types
+        of yaml, and leads to incompatibilities down the track.
+
+        Parameters
+        ----------
+        tag_to_remove : str
+            The tag to remove
+
+        Returns
+        -------
+        None
+        """
+        if not 'yaml_implicit_resolvers' in cls.__dict__:
+            cls.yaml_implicit_resolvers = cls.yaml_implicit_resolvers.copy()
+
+        for first_letter, mappings in cls.yaml_implicit_resolvers.items():
+            cls.yaml_implicit_resolvers[first_letter] = [(tag, regexp)
+                                                         for tag, regexp in mappings
+                                                         if tag != tag_to_remove]
+
+NoDatesSafeLoader.remove_implicit_resolver('tag:yaml.org,2002:timestamp')
+
 def read_yaml(filepath: str | Path) -> dict | list[dict]:
     """
     Read a yaml file and return a dict or a list of dict.
@@ -53,12 +89,12 @@ def read_yaml(filepath: str | Path) -> dict | list[dict]:
     """
     try:
         with open(filepath, "r", encoding="utf-8") as in_file:
-            data = yaml.load(in_file, Loader=yaml.SafeLoader)
+            data = yaml.load(in_file, Loader=NoDatesSafeLoader)
 
     # If yaml contains multiple documents
     except yaml.composer.ComposerError:
         with open(filepath, "r", encoding="utf-8") as in_file:
-            data = list(yaml.load_all(in_file, Loader=yaml.SafeLoader))
+            data = list(yaml.load_all(in_file, Loader=NoDatesSafeLoader))
     return data
 
 
