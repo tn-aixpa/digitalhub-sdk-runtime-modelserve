f88a300456944dd4e41fdcbad270f1f29a2d291e
EvangMM <matteomartini92@yahoo.it>
2024-10-31 12:36:22 +0100

    export src code not in b64 when export executable

--- runtimes/dbt/digitalhub_runtime_dbt/entities/function/dbt/entity.py
+++ runtimes/dbt/digitalhub_runtime_dbt/entities/function/dbt/entity.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 import typing
 
 from digitalhub.entities.function._base.entity import Function
+from digitalhub.utils.generic_utils import decode_string
+from digitalhub.utils.io_utils import write_text, write_yaml
+from digitalhub.utils.uri_utils import map_uri_scheme
 
 if typing.TYPE_CHECKING:
     from digitalhub_runtime_dbt.entities.function.dbt.spec import FunctionSpecDbt
@@ -31,3 +34,35 @@ class FunctionDbt(Function):
 
         self.spec: FunctionSpecDbt
         self.status: FunctionStatusDbt
+
+    def export(self, filename: str | None = None) -> str:
+        """
+        Export object as a YAML file.
+
+        Parameters
+        ----------
+        filename : str
+            Filename to export object to. If None, the object is exported to the
+            default filename.
+
+        Returns
+        -------
+        str
+        """
+        obj = self.to_dict()
+
+        # Strip base64 from source at following conditions:
+        # - base64 is not None
+        # - source is local path
+        base64 = obj.get("spec", {}).get("source", {}).get("base64")
+        source = obj.get("spec", {}).get("source", {}).get("source")
+        if base64 is not None and source is not None and map_uri_scheme(source) == "local":
+            obj["spec"]["source"].pop("base64")
+            src_pth = self._context().root / source
+            write_text(src_pth, decode_string(base64))
+
+        if filename is None:
+            filename = f"{self.ENTITY_TYPE}-{self.name}-{self.id}.yml"
+        pth = self._context().root / filename
+        write_yaml(pth, obj)
+        return str(pth)
--- runtimes/kfp/digitalhub_runtime_kfp/entities/workflow/kfp/entity.py
+++ runtimes/kfp/digitalhub_runtime_kfp/entities/workflow/kfp/entity.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 import typing
 
 from digitalhub.entities.workflow._base.entity import Workflow
+from digitalhub.utils.generic_utils import decode_string
+from digitalhub.utils.io_utils import write_text, write_yaml
+from digitalhub.utils.uri_utils import map_uri_scheme
 
 if typing.TYPE_CHECKING:
     from digitalhub_runtime_kfp.entities.workflow.kfp.spec import WorkflowSpecKfp
@@ -31,3 +34,35 @@ class WorkflowKfp(Workflow):
 
         self.spec: WorkflowSpecKfp
         self.status: WorkflowStatusKfp
+
+    def export(self, filename: str | None = None) -> str:
+        """
+        Export object as a YAML file.
+
+        Parameters
+        ----------
+        filename : str
+            Filename to export object to. If None, the object is exported to the
+            default filename.
+
+        Returns
+        -------
+        str
+        """
+        obj = self.to_dict()
+
+        # Strip base64 from source at following conditions:
+        # - base64 is not None
+        # - source is local path
+        base64 = obj.get("spec", {}).get("source", {}).get("base64")
+        source = obj.get("spec", {}).get("source", {}).get("source")
+        if base64 is not None and source is not None and map_uri_scheme(source) == "local":
+            obj["spec"]["source"].pop("base64")
+            src_pth = self._context().root / source
+            write_text(src_pth, decode_string(base64))
+
+        if filename is None:
+            filename = f"{self.ENTITY_TYPE}-{self.name}-{self.id}.yml"
+        pth = self._context().root / filename
+        write_yaml(pth, obj)
+        return str(pth)
