cfa51fd11a86bd6597fff427a6f061554782389f
EvangMM <matteomartini92@yahoo.it>
2024-11-05 16:35:19 +0100

    chore: set new options spec exporter

--- spec_exporter/exporter.sh
+++ spec_exporter/exporter.sh
@@ -5,15 +5,19 @@ if [ -z "$DTO" ] || [ -z "$KIND" ]; then
     echo "Error: DTO and KIND must be provided"
     exit 1
 fi
-
 BASE_API=http://192.168.49.2:30180/api/v1/schemas
-curl $BASE_API/$DTO/$KIND | jq -r ".schema" > ${DTO}_${KIND}.json
+curl $BASE_API/$DTO/$KIND --user  | jq -r ".schema" > ${DTO}_${KIND}.json
 datamodel-codegen   --input ${DTO}_${KIND}.json \
                     --input-file-type jsonschema \
                     --output ./ \
-                    --output-model-type pydantic.BaseModel \
+                    --output-model-type pydantic_v2.BaseModel \
+                    --enum-field-as-literal all \
                     --field-constraints \
-                    --target-python-version 3.9 \
-                    --use-standard-collection \
-                    --use-schema-description
+                    --use-generic-container-types \
+                    --use-standard-collections \
+                    --snake-case-field \
+                    --strip-default-none \
+                    --allow-extra-fields \
+                    --class-name Validator
+
 rm ${DTO}_${KIND}.json
