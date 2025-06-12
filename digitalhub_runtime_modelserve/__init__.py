4d678cac866d86bb31d6b507c0a5a8ed23406f65
EvangMM <matteomartini92@yahoo.it>
2025-06-12 09:54:16 +0200

    docs: added copyright header

--- digitalhub_runtime_modelserve/__init__.py
+++ digitalhub_runtime_modelserve/__init__.py
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: © 2025 DSLab - Fondazione Bruno Kessler
+#
 # SPDX-License-Identifier: Apache-2.0
 from digitalhub_runtime_modelserve.entities._commons.enums import EntityKinds
 from digitalhub_runtime_modelserve.entities.function.huggingfaceserve.builder import FunctionHuggingfaceserveBuilder
